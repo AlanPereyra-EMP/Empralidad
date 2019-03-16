51e0d899cfd4026d013b97136628c5e9969774eb
alanpereyra57 <alan@pereyra.online>
2019-03-16 15:37:29 -0300

    Add updates support and rewrite readme.md

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php 
+// 0) Updates
 // 1) Main menu
 // 2) Widgets
 // 3) Includes
@@ -10,6 +11,35 @@
 // 9) Wp link pages
 // 10) Custom post type
 
+//  0) Updates
+    // set_site_transient('update_themes', null);
+	function geko_check_update( $transient ) {
+	    if ( empty( $transient->checked ) ) {
+	        return $transient;
+	    }
+	    $theme_data = wp_get_theme(wp_get_theme()->template);
+	    $theme_slug = $theme_data->get_template();
+	    //Delete '-master' from the end of slug
+	    $theme_uri_slug = preg_replace('/-master$/', '', $theme_slug);
+	    
+	    $remote_version = '0.0.0';
+	    $style_css = wp_remote_get("https://raw.githubusercontent.com/alanpereyra57/".$theme_uri_slug."/master/style.css")['body'];
+	    if ( preg_match( '/^[ \t\/*#@]*' . preg_quote( 'Version', '/' ) . ':(.*)$/mi', $style_css, $match ) && $match[1] )
+	        $remote_version = _cleanup_header_comment( $match[1] );
+	   
+	    if (version_compare($theme_data->version, $remote_version, '<')) {
+	        $transient->response[$theme_slug] = array(
+	            'theme'       => $theme_slug,
+	            'new_version' => $remote_version,
+	            'url'         => 'https://github.com/alanpereyra57/'.$theme_uri_slug,
+	            'package'     => 'https://github.com/alanpereyra57/'.$theme_uri_slug.'/archive/master.zip',
+	        );
+	    }
+	       
+	    return $transient;
+	}
+	add_filter( 'pre_set_site_transient_update_themes', 'geko_check_update' );
+// 	end updates
 // 1) Main menu
 	if (function_exists('register_nav_menus')) {
 		register_nav_menus (array('superior' => 'Menu Principal'));
