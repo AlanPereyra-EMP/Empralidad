7b4bb5367746af36c051cd7e7b5830ac1c35d29b
alanpereyra57 <alan@pereyra.online>
2019-04-03 15:04:59 -0300

    fix card title

--- content.php
+++ content.php
@@ -1,12 +1,12 @@
 <div class="card p-0">
-	<div class="card-header"> <?php the_category(', '); ?> </div>
+    <div class="card-header"> <?php the_category(', '); ?> </div>
     <!-- body -->
         <div class="card-block">
     <!-- Img -->
         <?php if ( has_post_thumbnail() ) { ?>
-                <div class=" nopadding">
+                <div class="position-relative nopadding">
                     <?php the_post_thumbnail( 'full', array( 'class' => 'col-12 mx-auto h-auto brightness-04') );?>
-                    <div class="d-flex header-text1">
+                    <div class="carousel-caption d-flex header-text1">
                         <h2 class="container-fluid my-auto d-block text-white text-mobile">
                             <?php the_title(); ?>  
                         </h2>
@@ -18,9 +18,9 @@
                 </div>
         <?php } ?>
     <!-- end img -->
-        	<br>
+            <br>
             <div class="card-text p-3">
-            	<?php the_excerpt(); ?>
+                <?php the_excerpt(); ?>
             </div>
             <a href="<?php the_permalink(); ?>">
                 <button class="btn bg-personalized text-white container-fluid alingcenter mt-auto">
