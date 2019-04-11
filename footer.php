3d077726386b1d84ed91a1b64bfb62efd02374fc
alanpereyra57 <alan@pereyra.online>
2019-04-11 12:41:57 -0300

    upload correct screenshot.png

--- footer.php
+++ footer.php
@@ -33,7 +33,7 @@
 		            </div>
             	<!-- end social buttons -->
                 <!-- Owner -->
-                    <p><?php echo get_theme_mod('ip_footer_text'); ?><br><small>Desarrollado por <a href="https://okcomputer.online">Ok Computer</a></small></p>
+                    <p><?php echo get_theme_mod('ip_footer_text'); ?><br><small>Desarrollado por <a href="https://informatica.pereyra.online">InFormatica Pereyra</a></small></p>
                 <!-- end owner -->
                 <p><small><a href="<?php echo get_permalink(get_theme_mod('ip_headline_link_term')); ?>" class="text-secondary">Terminos y Condiciones</a> -  
                 <a href="<?php echo get_permalink(get_theme_mod('ip_headline_link_poli')); ?>" class="text-secondary">Politicas de Privacidad</a></small></p>
