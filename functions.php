83fc16c12fd6f85fd2e4eb67361760c23f6a7a74
alanpereyra57 <alan@pereyra.online>
2019-04-30 18:58:52 -0300

    re fix last commit

--- functions.php
+++ functions.php
@@ -71,7 +71,7 @@
 	?>
 	    <style type="text/css">
 	        a.button.product_type_simple.add_to_cart_button.ajax_add_to_cart, .dashboard-section-overview .copy-to-clipboard-trigger {
-	            background-color: <?php echo get_theme_mod('ip_title_color'); ?>;
+	            background-color: <?php echo get_theme_mod('ip_title_color'); ?>!important;
 	            color: white;
 	        }
 	        .bg-personalized, th,span.onsale, button.single_add_to_cart_button.button.alt, .reset_variations, #bg-menu-mobile, #first-content-home{
