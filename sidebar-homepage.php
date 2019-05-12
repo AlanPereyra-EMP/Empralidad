a62dab9a5bbdba7f769ed9bbb495ad5389ef9ed5
alanpereyra57 <alan@pereyra.online>
2019-05-12 16:33:46 -0300

    Add to-fade on hompage widget area

--- sidebar-homepage.php
+++ sidebar-homepage.php
@@ -1,4 +1,4 @@
-<div class="container-fluid my-5">
+<div class="container-fluid my-5 to-fade">
 	<div class="text-justify text-center">
 	<?php if ( is_active_sidebar( 'homepage' ) ) : ?>
 		<?php dynamic_sidebar( 'homepage' ); ?>
