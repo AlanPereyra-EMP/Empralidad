4f85cb4039b96652282e559e58f5a0d15b4e71ae
alanpereyra57 <alan@pereyra.online>
2019-06-06 16:07:25 -0300

    fix left sidebar bug

--- front-page.php
+++ front-page.php
@@ -139,94 +139,96 @@
         <!-- end sidebar front page -->
         <!-- The content -->
             <div class="row mw-100 mx-auto">
-                <div class="col-12 col-md-3 mx-auto px-0 bg-personalized show-from-md left-featured-text <?php if( is_admin_bar_showing() ){ ?> left-featured-text-admin <?php } ?>">
-                    <!-- Text 1 -->
-                        <?php $ip_homepage_show = get_theme_mod('ip_homepage_show1');
-                        $logo = get_theme_mod('ip_img_logo');
+                <?php $ip_show1 = get_theme_mod('ip_homepage_show1'); ?>
+                <?php $ip_show2 = get_theme_mod('ip_homepage_show2'); ?>
+                <?php $ip_show3 = get_theme_mod('ip_homepage_show3'); ?>
+                <?php if($ip_show1 === 'true'||$ip_show2 === 'true'||$ip_show3 === 'true'){ ?>
+                    <div class="col-12 col-md-3 mx-auto px-0 bg-personalized min-h-100 show-from-md left-featured-text <?php if( is_admin_bar_showing() ){ ?> left-featured-text-admin <?php } ?>">
+                        <!-- Text 1 -->
+                            <?php $ip_homepage_show = get_theme_mod('ip_homepage_show1');
+                            $logo = get_theme_mod('ip_img_logo');
 
-                        if ($ip_homepage_show == 'true'){ 
-                            $img = get_theme_mod('ip_homepage_img1'); ?>
-                            <div class="py-3 text-white col-12 mx-auto my-auto text-shadow-6" >
-                                <h2 class="text-center">
-                                    <span class="text-white"><?php echo get_theme_mod('ip_homepage_title1') ?></span>
-                                </h2>
-                                <p class="p-2 mw-100 text-<?php echo get_theme_mod('ip_homepage_text_aling1'); ?> " >
-                                    <?php if($img){ ?> <img src="<?php echo wp_get_attachment_url(get_theme_mod('ip_homepage_img1')); ?>" class="img-fluid"> <?php } ?> <br>
-                                    <?php echo get_theme_mod('ip_homepage_text1') ?>
-                                </p>
-                                
-                                <?php $header_btn = get_theme_mod('ip_homepage_btn1');
-
-                                if ($header_btn){ ?>
-                                    <p class="text-center">
-                                        <button onclick="window.location.href = '<?php echo get_theme_mod('ip_homepage_link_btn1') ?>';" class="container-fluid btn bg-light text-dark">
-                                            <?php echo get_theme_mod('ip_homepage_btn1') ?>
-                                        </button>
+                            if ($ip_homepage_show == 'true'){ 
+                                $img = get_theme_mod('ip_homepage_img1'); ?>
+                                <div class="py-3 text-white col-12 mx-auto my-auto text-shadow-6" >
+                                    <h2 class="text-center">
+                                        <span class="text-white"><?php echo get_theme_mod('ip_homepage_title1') ?></span>
+                                    </h2>
+                                    <p class="p-2 mw-100 text-<?php echo get_theme_mod('ip_homepage_text_aling1'); ?> " >
+                                        <?php if($img){ ?> <img src="<?php echo wp_get_attachment_url(get_theme_mod('ip_homepage_img1')); ?>" class="img-fluid"> <?php } ?> <br>
+                                        <?php echo get_theme_mod('ip_homepage_text1') ?>
                                     </p>
-                                <?php } ?>
-                            </div>
-                        <?php } ?>
-                    <!-- end text 1 (featured) -->
-                    <!-- Text 2 -->
-                    <?php $ip_homepage_show = get_theme_mod('ip_homepage_show2');
+                                    
+                                    <?php $header_btn = get_theme_mod('ip_homepage_btn1');
 
-                    if ($ip_homepage_show == 'true'){ 
-                        $img = get_theme_mod('ip_homepage_img2'); ?>
-                        <div class="py-3 text-white col-12 mx-auto my-auto text-shadow-6">
-                            <h2 class="text-center">
-                                <span class="text-white"><?php echo get_theme_mod('ip_homepage_title2') ?></span>
-                            </h2>
-                            <p class="p-2 mw-100 text-<?php echo get_theme_mod('ip_homepage_text_aling2'); ?> " >
-                                <?php if($img){ ?> <img src="<?php echo wp_get_attachment_url(get_theme_mod('ip_homepage_img2')); ?>" class="img-fluid"> <?php } ?> <br>
-                                <?php echo get_theme_mod('ip_homepage_text2') ?>
-                            </p>
-                            <?php $header_btn = get_theme_mod('ip_homepage_btn2');
-
-                            if ($header_btn){
-                                ?>
-                                <p class="text-center">
-                                    <button onclick="window.location.href = '<?php echo get_theme_mod('ip_homepage_link_btn2') ?>';" class="container-fluid btn bg-light text-dark">
-                                        <?php echo get_theme_mod('ip_homepage_btn2') ?>
-                                    </button>
-                                </p>
+                                    if ($header_btn){ ?>
+                                        <p class="text-center">
+                                            <button onclick="window.location.href = '<?php echo get_theme_mod('ip_homepage_link_btn1') ?>';" class="container-fluid btn bg-light text-dark">
+                                                <?php echo get_theme_mod('ip_homepage_btn1') ?>
+                                            </button>
+                                        </p>
+                                    <?php } ?>
+                                </div>
                             <?php } ?>
-                        </div>
-                    <?php } ?>
-                    <!-- end text 2 -->
-                    <!-- Text 3 -->
-                        <?php $ip_homepage_show = get_theme_mod('ip_homepage_show3');
+                        <!-- end text 1 (featured) -->
+                        <!-- Text 2 -->
+                        <?php $ip_homepage_show = get_theme_mod('ip_homepage_show2');
 
                         if ($ip_homepage_show == 'true'){ 
-                            $img = get_theme_mod('ip_homepage_img3'); ?>
-                            <div class="py-3 text-white col-12 mx-auto my-auto text-shadow-6" >
-                                <h2 class="text-center" >
-                                    <span class="text-white"><?php echo get_theme_mod('ip_homepage_title3'); ?></span>
+                            $img = get_theme_mod('ip_homepage_img2'); ?>
+                            <div class="py-3 text-white col-12 mx-auto my-auto text-shadow-6">
+                                <h2 class="text-center">
+                                    <span class="text-white"><?php echo get_theme_mod('ip_homepage_title2') ?></span>
                                 </h2>
-                                <p class="p-2 mw-100 text-<?php echo get_theme_mod('ip_homepage_text_aling3'); ?> " >
-                                    <?php if($img){ ?> <img src="<?php echo wp_get_attachment_url(get_theme_mod('ip_homepage_img3')); ?>" class="img-fluid"> <?php } ?> <br>
-                                    <?php echo get_theme_mod('ip_homepage_text3') ?>
+                                <p class="p-2 mw-100 text-<?php echo get_theme_mod('ip_homepage_text_aling2'); ?> " >
+                                    <?php if($img){ ?> <img src="<?php echo wp_get_attachment_url(get_theme_mod('ip_homepage_img2')); ?>" class="img-fluid"> <?php } ?> <br>
+                                    <?php echo get_theme_mod('ip_homepage_text2') ?>
                                 </p>
-                                <?php 
-                                $header_btn = get_theme_mod('ip_homepage_btn3');
+                                <?php $header_btn = get_theme_mod('ip_homepage_btn2');
 
-                                if ($header_btn){ ?>
+                                if ($header_btn){
+                                    ?>
                                     <p class="text-center">
-                                        <button onclick="window.location.href = '<?php echo get_theme_mod('ip_homepage_link_btn3') ?>';" class="container-fluid btn bg-light text-dark">
-                                            <?php echo get_theme_mod('ip_homepage_btn3') ?>
+                                        <button onclick="window.location.href = '<?php echo get_theme_mod('ip_homepage_link_btn2') ?>';" class="container-fluid btn bg-light text-dark">
+                                            <?php echo get_theme_mod('ip_homepage_btn2') ?>
                                         </button>
                                     </p>
                                 <?php } ?>
                             </div>
                         <?php } ?>
-                    <!-- end text 3 -->
-                </div>
+                        <!-- end text 2 -->
+                        <!-- Text 3 -->
+                            <?php $ip_homepage_show = get_theme_mod('ip_homepage_show3');
+
+                            if ($ip_homepage_show == 'true'){ 
+                                $img = get_theme_mod('ip_homepage_img3'); ?>
+                                <div class="py-3 text-white col-12 mx-auto my-auto text-shadow-6" >
+                                    <h2 class="text-center" >
+                                        <span class="text-white"><?php echo get_theme_mod('ip_homepage_title3'); ?></span>
+                                    </h2>
+                                    <p class="p-2 mw-100 text-<?php echo get_theme_mod('ip_homepage_text_aling3'); ?> " >
+                                        <?php if($img){ ?> <img src="<?php echo wp_get_attachment_url(get_theme_mod('ip_homepage_img3')); ?>" class="img-fluid"> <?php } ?> <br>
+                                        <?php echo get_theme_mod('ip_homepage_text3') ?>
+                                    </p>
+                                    <?php 
+                                    $header_btn = get_theme_mod('ip_homepage_btn3');
+
+                                    if ($header_btn){ ?>
+                                        <p class="text-center">
+                                            <button onclick="window.location.href = '<?php echo get_theme_mod('ip_homepage_link_btn3') ?>';" class="container-fluid btn bg-light text-dark">
+                                                <?php echo get_theme_mod('ip_homepage_btn3') ?>
+                                            </button>
+                                        </p>
+                                    <?php } ?>
+                                </div>
+                            <?php } ?>
+                        <!-- end text 3 -->
+                    </div>
+                <?php } ?>
                 <?php if ( is_front_page() && !is_home() ) {?>
                     <?php get_template_part('content-home') ?>
                 <?php } else { ?> 
-                    <?php $ip_show1 = get_theme_mod('ip_homepage_show1'); ?>
-                    <?php $ip_show2 = get_theme_mod('ip_homepage_show2'); ?>
-                    <?php $ip_show3 = get_theme_mod('ip_homepage_show3'); ?>
-                    <article class="pt-5 p-3 col-12 <?php if($ip_show1||$ip_show2||$ip_show3){ ?> col-md-9 <?php }else{ ?> col-md-10 col-lg-9 <?php } ?> mx-auto">
+                    <article class="pt-5 p-3 col-12 <?php if($ip_show1 === 'true'||$ip_show2 === 'true'||$ip_show3 === 'true'){ ?> col-md-9 <?php }else{ ?> col-md-10 col-lg-9 <?php } ?> mx-auto">
                         <h2 class="text-center">Lo más reciente</h2>
                         <div class="card-columns-2 p-2">
                             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
