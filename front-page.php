41fd7036beb3b5b0849f12e78a9f5de3d55c5d7e
alanpereyra57 <alan@pereyra.online>
2019-06-15 21:45:06 -0300

    to do improve woocommerce featured products

--- front-page.php
+++ front-page.php
@@ -125,7 +125,7 @@
                 if($featured_products === 'true') { ?>
                     <h3 class="pt-5 pb-4 widget-title-dark text-center"><?php echo get_theme_mod('ip_woocommerce_featured_title'); ?></h3>
                     <p class="py-2 text-center"><small><?php echo get_theme_mod('ip_woocommerce_featured_text'); ?></small></p>
-                    <div class="p-3 col-12 col-md-10 col-lg-9 mx-auto text-center">
+                    <div id="woo_featured_products" class="p-3 col-12 col-md-10 col-lg-9 mx-auto text-center" style="overflow-x: auto;">
                         <?php echo do_shortcode('[products]'); ?>
                     </div>
                     <br><br>
