652c2a0dd89a03fd34d69c44dd145b88314c6bf3
alanpereyra57 <alan@pereyra.online>
2019-06-18 18:36:30 -0300

    overflow-y:auto; -> overflow:auto;

--- front-page.php
+++ front-page.php
@@ -124,12 +124,11 @@
                 <?php if (class_exists('WooCommerce')){
                     $featured_products = get_theme_mod('ip_woocommerce_featured_show');
                     if($featured_products === 'true') { ?>
-                        <h3 class="pt-5 pb-4 widget-title-dark text-center"><?php echo get_theme_mod('ip_woocommerce_featured_title'); ?></h3>
+                        <h2 class="pt-5 pb-4 widget-title-dark text-center"><?php echo get_theme_mod('ip_woocommerce_featured_title'); ?></h2>
                         <p class="py-2 text-center"><small><?php echo get_theme_mod('ip_woocommerce_featured_text'); ?></small></p>
                         <div id="woo_featured_products" class="p-3 col-12 col-md-10 col-lg-9 mx-auto text-center" style="overflow-x: auto;">
                             <?php echo do_shortcode('[featured_products columns="3" orderby="price" order="ASC"]'); ?>
                         </div>
-                        <br><br>
                     <?php } 
                 } ?>
             </div>
