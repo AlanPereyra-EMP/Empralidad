7eeaf62ee66dd47495f5831cfc7e6d21dc1944ef
alanpereyra57 <alan@pereyra.online>
2019-09-29 10:27:28 -0300

    fix sidebar homepage1

--- sidebar-homepage1.php
+++ sidebar-homepage1.php
@@ -1,7 +1,7 @@
 <div class="container-fluid my-5 to-fade to-fadein-animation">
 	<div class="text-justify text-center">
-	<?php if ( is_active_sidebar( 'homepage' ) ) : ?>
-		<?php dynamic_sidebar( 'homepage' ); ?>
+	<?php if ( is_active_sidebar( 'homepage1' ) ) : ?>
+		<?php dynamic_sidebar( 'homepage1' ); ?>
 	<?php endif; ?>
 	</div>
 </div>
