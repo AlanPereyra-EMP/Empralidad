3fb46788b71b47f19f45c2e52f70d3e172af8cb9
alanpereyra57 <alan@pereyra.online>
2019-11-11 05:13:14 -0300

    add Tutor LMS support

--- functions.php
+++ functions.php
@@ -101,7 +101,7 @@
 	            background-color: <?php echo get_theme_mod('ip_btn_bg'); ?>!important;
 	            color: <?php echo get_theme_mod('ip_btn_color'); ?> !important;
 	        }
-	        .bg-personalized, th,span.onsale, .reset_variations, #bg-menu-mobile, #first-content-home, .bg-title-color, .bg-title-content-color, #main-head{
+	        .bg-personalized, th,span.onsale, .reset_variations, #bg-menu-mobile, #first-content-home, .bg-title-color, .bg-title-content-color, #main-head, .tutor-wrap.tutor-full-width-course-top, .tutor-single-page-top-bar{
 	        	background-color: <?php echo get_theme_mod('ip_primary_color'); ?> !important;
 	        }
 	        strong,h2, h3, h4, h5, h6, .widget-title-dark, .color-personalized, .product_title{
