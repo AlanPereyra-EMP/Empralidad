5e2b6385fc92061c9de99b51a20d1c2a3b59edbd
alanpereyra57 <alan@pereyra.online>
2019-12-17 04:44:49 -0300

    improve tutor ip design

--- functions.php
+++ functions.php
@@ -111,11 +111,16 @@
 	        	color: <?php echo get_theme_mod('ip_primary_color'); ?> !important;
 	        }
 					.tutor-wrap, .tutor-single-lesson-wrap{
-						border-top: 60px solid <?php echo get_theme_mod('ip_primary_color'); ?> !important;
+						border-top: 70px solid <?php echo get_theme_mod('ip_primary_color'); ?> !important;
 					}
 	        .ip_img_slide1{
 	        	background: url(<?php echo wp_get_attachment_url(get_theme_mod('ip_head_slide1')); ?>) no-repeat 50% 50%;
 	        }
+					.tutor-wrap.tutor-courses-wrap.tutor-container {
+					    margin: 0;
+					    max-width: inherit;
+					    min-height: 75vh;
+					}
 	        .btn, .wpcf7-submit, #submit.submit, .woocommerce-cart .wc-proceed-to-checkout a.checkout-button{
 	        	background-color: <?php echo get_theme_mod('ip_btn_bg'); ?>;
 	        	color: <?php echo get_theme_mod('ip_btn_color'); ?>;
