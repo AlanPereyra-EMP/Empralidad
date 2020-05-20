fb1f116618b2d15d0fd4d4139c125132b78199e9
alanpereyra57 <alan@pereyra.online>
2020-05-20 18:06:36 -0300

    Fix has-background text color

--- woocommerce.php
+++ woocommerce.php
@@ -5,7 +5,7 @@
     <!-- Title -->
         <div class="container-fluid pt-5 pb-3 text-center text-justify">
         </div>
-        <h1 class="color-personalized text-center py-5 px-2 px-md-5 text-mobile"><?php the_title(); ?></h1>
+        <h1 class="color-personalized text-center py-5 px-2 px-md-5 text-mobile">Tienda</h1>
     <!-- end title -->
     <!-- Page content -->
         <article class="container my-5 p-2 p-md-4 bg-personalized-secondary">
