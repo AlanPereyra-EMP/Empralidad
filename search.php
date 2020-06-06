8c29372c158759af25310b87f0d8acdb94e66f13
alanpereyra57 <alan@pereyra.online>
2020-06-06 17:23:15 -0300

    fix 404 and search pages

--- search.php
+++ search.php
@@ -1,18 +1,21 @@
 <?php get_header(); ?>
 <!-- Title -->
-    <div class="container-fluid py-5 text-center pt-105 pt-lg-90 text-justify">
-        <h1 class="color-personalized">Resultados de busqueda</h1>
+    <div class="container-fluid py-5 text-center  pt-105 pt-lg-90 text-justify">
+        <h1 class="color-personalized text-mobile">Has buscado</h1>
     </div>
 <!-- end title -->
 <!-- Content -->
     <section>
         <?php if ( ! have_posts() ) {?>
-            <div class="col-11 col-md-8 mx-auto py-10 px-5 bg-white border-30px">
-            <h6>No se encontro lo que buscás</h6>
-            <p>Escribilo de otra manera</p><br>
-            <?php get_search_form(); ?>
+            <div class="col-11 col-md-8 mx-auto py-10 px-5 color-personalized border-30px">
+              <?php get_search_form(); ?><br>
+              <h2>Pero no se encontro dentro de esta web</h2>
+              <p>Escribilo de otra manera</p>
             </div>
         <?php } else { ?>
+            <div class="mb-5">
+              <?php get_search_form(); ?>
+            </div>
             <div class="card-columns-2 p-2">
                 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                     <?php get_template_part('content', get_post_format()) ?>
