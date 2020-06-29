85eb0385a45c608301d3faef9a29d57f5e20b654
alanpereyra57 <alan@pereyra.online>
2020-06-29 13:48:43 -0300

    Text center content home

--- content-home.php
+++ content-home.php
@@ -2,7 +2,7 @@
 <?php $emp_show2 = get_theme_mod('emp_homepage_show2'); ?>
 <?php $emp_show3 = get_theme_mod('emp_homepage_show3'); ?>
 
-	<h2 class="px-3 h1 widget-title-dark"><strong class=""><?php the_title(); ?></strong>	 </h2>
+	<h2 class="px-3 h1 widget-title-dark text-center"><strong class=""><?php the_title(); ?></strong>	 </h2>
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
 		    the_content();
 		endwhile; endif; ?>
