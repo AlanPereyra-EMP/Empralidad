0e51cd57c690dcce24a4f6377cb2120d5fa3d34b
Alan Pereyra <alanpereyra57@gmail.com>
2020-12-02 11:07:43 -0300

    fix woocommerce products img size

--- woocommerce.php
+++ woocommerce.php
@@ -40,14 +40,36 @@
     padding-top: 100px;
     margin-top: -100px;
   }
-  img.attachment-woocommerce_thumbnail.size-woocommerce_thumbnail {
-    height: 215px!important;
-  }
   li.product.type-product {
     height: 350px;
   }
   .product img.faded {
     border-radius: 0!important;
+  }
+  img.attachment-woocommerce_thumbnail.size-woocommerce_thumbnail.faded.lazyloaded {
+    position: absolute;
+    bottom: 70%;
+    transform: translate(0, 50%);
+    max-height: 215px;
+    height: auto!important;
+  }
+  li.product span.price {
+    position: absolute!important;
+    bottom: 15%!important;
+    left: 50%!important;
+    transform: translate(-50%, 0)!important;
+  }
+  h2.woocommerce-loop-product__title.faded {
+    position: absolute;
+    bottom: 32%;
+    width: 100%;
+    transform: translate(0, 50%);
+  }
+  .woocommerce ul.products li.product .price{
+    margin-bottom: 0!important;
+  }
+  .woocommerce .products .star-rating{
+    margin: 7px 0 .5em;
   }
 </style>
 <div class="container-fluid py-5"></div>
