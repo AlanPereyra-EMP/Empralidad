1c36f00d53d7fe1ca9b8599c49da2237594674e0
Alan Pereyra <alanpereyra33@gmail.com>
2021-01-11 03:01:06 -0300

    delete sfpo.css and minify plugins support

--- includes/enqueue-scripts.php
+++ includes/enqueue-scripts.php
@@ -3,7 +3,6 @@
 function emp_styles(){
   wp_enqueue_style ('emp_styles', get_template_directory_uri() . '/style.css');
   wp_enqueue_style ('emp_plugin-support', get_template_directory_uri() . '/includes/css/plugins-support.css');
-  wp_enqueue_style ('emp_sfpo', get_template_directory_uri() . '/includes/css/sfpo.css');
   wp_enqueue_style ('font_awesome', get_template_directory_uri() . '/includes/fonts/fa/css/all.min.css');
 }
 add_action('wp_enqueue_scripts', 'emp_styles');
