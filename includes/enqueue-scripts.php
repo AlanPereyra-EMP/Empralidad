9000477addf46479afb677d5c17d699292bc39d6
Alan Pereyra <alanpereyra33@gmail.com>
2021-01-11 05:14:24 -0300

    move enqueue scripts on footer again

--- includes/enqueue-scripts.php
+++ includes/enqueue-scripts.php
@@ -18,11 +18,13 @@ add_action('wp_enqueue_scripts', 'emp_scripts');
 function emp_scripts_footer() {
   remove_action('wp_head', 'wp_print_scripts');
   remove_action('wp_head', 'wp_print_head_scripts', 9);
+  remove_action('wp_head', 'wp_enqueue_scripts', 1);
 
   add_action('wp_footer', 'wp_print_scripts', 5);
+  add_action('wp_footer', 'wp_enqueue_scripts', 5);
   add_action('wp_footer', 'wp_print_head_scripts', 5);
 }
-add_action('wp_enqueue_scripts', 'emp_scripts_footer');
+add_action( 'wp_enqueue_scripts', 'emp_scripts_footer' );
 
 function emp_deregister_styles() {
   wp_deregister_style('wc-block-style');
