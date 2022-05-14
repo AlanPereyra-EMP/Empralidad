d9eac07ee2733b45c88b5a9fb5e54826a33ac23c
Alan Pereyra <alanpereyra33@gmail.com>
2022-05-13 21:39:33 -0300

    fix featured product style when negative margin is active

--- includes/wc-featured-products.php
+++ includes/wc-featured-products.php
@@ -4,9 +4,11 @@
 <?php if($featured_products && class_exists('WooCommerce')) { ?>
   <div class="p-2 <?php if($featured_products_margin_negative){ ?> mt-250-n <?php } ?>">
     <?php if($featured_products_h2){ ?>
-      <h2 class="pt-5 pb-4 px-0 h1 text-center"><?php echo get_theme_mod('emp_woocommerce_featured_title'); ?></h2>
+      <div class="<?php if($featured_products_margin_negative){ ?> title-color <?php } ?>">
+        <h2 class="pt-5 pb-4 px-0 h1 text-center"><?php echo get_theme_mod('emp_woocommerce_featured_title'); ?></h2>
+        <p class="py-2 text-center"><small><?php echo get_theme_mod('emp_woocommerce_featured_text'); ?></small></p>
+      </div>
     <?php } ?>
-    <p class="py-2 text-center"><small><?php echo get_theme_mod('emp_woocommerce_featured_text'); ?></small></p>
     <div id="woo_featured_products" class="px-1 py-4 col-12 mx-auto text-center overflow-hidden">
       <?php echo do_shortcode('[featured_products columns="3" orderby="price" order="ASC"]'); ?>
     </div>
