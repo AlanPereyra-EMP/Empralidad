34266c41a5c9ab6d3c5d6156e99ac91084e2cf88
Alan Pereyra <alanpereyra33@gmail.com>
2022-06-20 01:16:08 -0300

    Add courses shortcode

--- plugins/emp-shortcodes.php
+++ plugins/emp-shortcodes.php
@@ -121,123 +121,51 @@ if(!shortcode_exists('emp_table')) {
 // Resent posts
 if(!shortcode_exists('emp_posts')) {
   function emp_postsbycategory($atts) {
+    $args = array(
+      'post_type' => 'emp_courses',
+      'post_status' => 'publish',
+      'posts_per_page' => 8,
+      'orderby' => 'title',
+      'order' => 'ASC',
+    );
 
-    $attributes = shortcode_atts( array(
-      'posts' => '4',
-      'category' => ''
-    ), $atts );
+    $loop = new WP_Query( $args );
 
-      $args = array(
-        'posts_per_page' => $attributes['posts'],
-        'offset' => 0,
-        'category_name' => $attributes['category'],
-        'orderby' => 'ID',
-        'order' => 'DESC',
-        'post_status' => 'publish',
-        'suppress_filters' => true
-      );
-
-          $the_query = new WP_Query( $args );
+    while ( $loop->have_posts() ) : $loop->the_post();
+    $the_category = get_the_category_list(', ');
+    $permalink = get_the_permalink();
+    $the_title = get_the_title();
+    $the_excerpt = get_the_excerpt();
 
 
+      if ( has_post_thumbnail() ) {
+        $image = '<a href="'.$permalink.'">
+                    <div class="position-relative nopadding">
+                      '.get_the_post_thumbnail( '', 'full', array( 'class' => 'col-12 mx-auto h-auto no-border-radius') ).'
+                    </div>
+                  </a>';
+      }
 
-          if ( $the_query->have_posts() ) {
-              while ( $the_query->have_posts() ) { $the_query->the_post();
-
-                $post_format = get_post_format();
-
-                $post_categories = get_the_category();
-                $post_category = esc_html( $post_categories[0]->name );
-                $post_category_id = get_cat_ID( $post_category );
-                $post_category_link = get_category_link( $post_category_id );
-
-                $post_filter = get_theme_mod('emp_img_filter1');
-                $post_thumbnail = get_the_post_thumbnail();
-                $post_permalink = get_the_permalink();
-                $post_title = get_the_title();
-                $post_excerpt = get_the_excerpt();
-                $post_date = get_the_date();
-                $post_author = get_the_author();
-                $post_tags = get_the_tags();
-                $post_content = get_the_content();
-
-
-                if ($post_filter === 'color') {
-                  $post_filter = 'bg-title-content-color';
-                }else{
-                  $post_filter = 'bg-title-content-dark';
-                }
-
-
-                $posts .= '<section class="card p-0 to-fade to-fadein-animation">
-                    <div class="card-header"><a href="'.$post_category_link.'">'. $post_category .'</a> </div>
-                    <!-- body -->
-                        <div class="card-block">';
-
-
-                if ($post_format === 'image' || $post_format === 'video'){
-                  $posts .= '<a href="'.$post_permalink.'">'.
-                                $post_thumbnail.'
-                            </a>
-                            <div class="card-block">
-                                <a href="'.$post_permalink.'"><button class="btn text-white container-fluid alingcenter mt-auto">Comentarios</button></a>
-                            </div>';
-                } else if ($post_format === 'image'){
-
-                } else if ($post_format === 'video'){
-
-                } else {
-                  if ( has_post_thumbnail() ) {
-                    $posts .= '<div class="position-relative nopadding">
-                                  '.$post_thumbnail.'
-                                  <div class="carousel-caption d-flex header-text1 h-100 '.$post_filter.'"></div>
-                                  <div class="carousel-caption d-flex header-text1">
-                                      <h2 class="container-fluid my-auto d-block text-white mh-100-auto">
-                                          <a href="'.$post_permalink.'"><h2 class="text-white">'.$post_title.'</h2></a>
-                                      </h2>
-                                  </div>
-                              </div>';
-                  } else {
-                    $posts .= '<div class=" py-5 text-center text-justify text-white bg-personalized">
-                                <a href="'.$post_permalink.'"><h2 class="text-white">'.$post_title.'</h2></a>
-                              </div>';
-                  }
-
-                }
-                if ($post_format === 'audio'){
-                  $posts .=  '<div class="card-text p-3 audio-content">'.
-                          $post_content.'
-                      </div>
-                  </div>';
-
-                } else if ($post_format === 'image'){
-
-                } else if ($post_format === 'video'){
-
-                } else {
-                  $posts .= '<br>
-                      <div class="card-text p-3">'.
-                          $post_excerpt.'
-                      </div>
-                      <a href="'.$post_permalink.'">
-                          <button class="btn text-white container-fluid alingcenter mt-auto">
-                              Leer más
-                          </button>
+      $courses = '<section class="card p-0 border-30px content-background content-color">
+                    <div class="card-header">'.$the_category.'</div>
+                    <div class="card-block">
+                      <?php'
+                      .$image.
+                      '<br>
+                      <h2 class="content-color">'.$the_title.'</h2>
+                      <div class="card-text p-3">'
+                        .$the_excerpt.
+                      '</div>
+                      <a href="'.$permalink.'">
+                        <button class="card-button btn container-fluid alingcenter mt-auto my-5">
+                          Ver más
+                        </button>
                       </a>
-                  </div>';
-                }
+                    </div>
+                  </section>';
+      endwhile;
 
-
-                $posts .= '<div class="card-footer text-muted">
-                            <small>'.$post_date.' / Autor: '.$post_author.' / '.$post_tags.' </small>
-                        </div>
-                    <!-- end body -->
-                </section>';
-              }
-          }
-          wp_reset_postdata();
-
-      return '<div class="card-columns-2">'.$posts.'</div>';
+      return '<div class="card-columns-2">'.$courses.'</div>';
 
   }
 
