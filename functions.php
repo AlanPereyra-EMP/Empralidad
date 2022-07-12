273f2578d2d3e6af601513df0197120a0435be8c
Alan Pereyra <alanpereyra33@gmail.com>
2022-07-12 17:42:46 -0300

    Menu mobile auto dropdown when is clicked

--- functions.php
+++ functions.php
@@ -350,45 +350,4 @@ add_filter( 'pre_set_site_transient_update_themes', 'emp_check_update' );
 if ( ! current_user_can( 'manage_options' ) ) {
   show_admin_bar( false );
 }
-
-function wporg_add_custom_box() {
-    $screens = [ 'emp_courses', 'wporg_cpt' ];
-    foreach ( $screens as $screen ) {
-        add_meta_box(
-            'wporg_box_id',                 // Unique ID
-            'Video presentación',      // Box title
-            'emp_custom_box_html',  // Content callback, must be of type callable
-            $screen                            // Post type
-        );
-    }
-}
-add_action( 'add_meta_boxes', 'wporg_add_custom_box' );
-
-function emp_custom_box_html( $post ) {
-	$value = get_post_meta( $post->ID, '_wporg_meta_key', true );
-    ?>
-		<!-- <label for="upload_video">
-			<input id="upload_video" type="text" size="36" name="ad_video" value="<?php echo $attachment; ?>" />
-			<input id="upload_video_button" class="button" type="button" value="Upload Image" />
-			<br>Elige un video para presentar el curso
-		</label> -->
-
-    <label for="wporg_field">Description for this field</label>
-    <select name="wporg_field" id="wporg_field" class="postbox">
-        <option value="">Select something...</option>
-        <option value="something" <?php selected( $value, 'something' ); ?>>Something</option>
-        <option value="else" <?php selected( $value, 'else' ); ?>>Else</option>
-    </select>
-    <?php
-}
-function wporg_save_postdata( $post_id ) {
-    if ( array_key_exists( 'wporg_field', $_POST ) ) {
-        update_post_meta(
-            $post_id,
-            '_wporg_meta_key',
-            $_POST['wporg_field']
-        );
-    }
-}
-add_action( 'save_post', 'wporg_save_postdata' );
 ?>
