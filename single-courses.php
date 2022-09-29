ebffb2d976d7e4993c1dbffe7ae4e4f8c2d86707
Alan Pereyra <alanpereyra33@gmail.com>
2022-09-29 19:13:47 -0300

    Fix bug

--- single-courses.php
+++ single-courses.php
@@ -66,7 +66,7 @@ $teacher_permalink = get_permalink($teacher_id);
                 if (wc_customer_bought_product( '', get_current_user_id(), $product->emp_product_courses)) {
                   $link = get_permalink($topic);
                 }else{
-                  $link = '#course-icons'
+                  $link = '#course-icons';
                 }
 
                 $title = get_the_title($topic);
