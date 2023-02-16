cd673e268b80857f8695f85abd9dcc1236d65ff6
Alan Pereyra <alanpereyra33@gmail.com>
2023-02-16 19:41:51 -0300

    Delete p-2 class on woocommerce page

--- woocommerce.php
+++ woocommerce.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<article class="my-5 p-2 p-md-4 content-background content-color woo-page-margin" style="">
+<article class="my-5 p-md-4 content-background content-color woo-page-margin" style="">
   <?php get_product_search_form(); ?>
   <?php woocommerce_content();?>
 </article>
