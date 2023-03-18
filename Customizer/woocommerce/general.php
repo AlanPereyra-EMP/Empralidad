55f0ecf473c05be0784f0b98acd75b1d28379f2a
Alan Pereyra <alanpereyra33@gmail.com>
2023-03-17 23:04:31 -0300

    fix new section on customizer

--- Customizer/woocommerce/general.php
+++ Customizer/woocommerce/general.php
@@ -1,22 +1,28 @@
 <?php
-$wp_customize->add_setting('emp_whatsapp_color', array(
+$wp_customize->add_section('emp_woocommerce_general', array(
+  'title' => __('Configuraciones generales', 'empralidad'),
+  'theme_supports'=> array('woocommerce'),
+  'priority'      => 3,
+  'panel'         => 'woocommerce'
+));
+$wp_customize->add_setting('emp_woocommerce_color', array(
   'default'          => '#000000',
   'trasnport'        => 'refresh',
   'sanitize_callback'=> 'sanitize_string'
 ));
-$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'emp_whatsapp_color_control', array(
+$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'emp_woocommerce_color_control', array(
   'label'   => __('Color de texto', 'empralidad'),
-  'panel'   => 'woocommerce',
-  'settings'=> 'emp_whatsapp_color'
+  'section'   => 'emp_woocommerce_general',
+  'settings'=> 'emp_woocommerce_color'
 )));
-$wp_customize->add_setting('emp_whatsapp_bg', array(
+$wp_customize->add_setting('emp_woocommerce_bg', array(
   'default'          => '#ffffff',
   'trasnport'        => 'refresh',
   'sanitize_callback'=> 'sanitize_string'
 ));
-$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'emp_whatsapp_bg_control', array(
+$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'emp_woocommerce_bg_control', array(
   'label'   => __('Color de fondo', 'empralidad'),
-  'panel'   => 'woocommerce',
-  'settings'=> 'emp_whatsapp_bg'
+  'section'   => 'emp_woocommerce_general',
+  'settings'=> 'emp_woocommerce_bg'
 )));
 ?>
