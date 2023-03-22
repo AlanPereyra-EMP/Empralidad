9a98c7b151bb18c0b0c579335c55c2c7458eb64c
alanpereyra57 <alanpereyra57@gmail.com>
2023-03-22 13:14:26 -0300

    Fix bug at product filter by price or name on shop page

--- woocommerce.php
+++ woocommerce.php
@@ -21,7 +21,7 @@ $lowest_price = get_post_meta( $lowest->ID, '_price', true );
     <div class="show-desktop">
       <p class="px-0 pb-0">Precio máximo</p>
       <small id="emp-small-price-desktop"><?php echo $highest_price; ?></small>
-      <input id="emp-range-price-desktop" class="emp-range-price mb-3" type="range" min="<?php echo $lowest_price; ?>" max="<?php echo $highest_price; ?>" step="1" onchange="window.location.href = 'http://localhost/wordpress/tienda/?max_price='+this.value">
+      <input id="emp-range-price-desktop" class="emp-range-price mb-3" type="range" min="<?php echo $lowest_price; ?>" max="<?php echo $highest_price; ?>" step="1" onchange="window.location.href =  window.location.href.split('?')[0]+'?max_price='+this.value">
       <p class="px-0 pb-0">Filtrar por nombre</p>
       <form role="search" method="get" class="woocommerce-product-search" action="<?php echo esc_url( home_url( '/'  ) ); ?>">
         <label class="screen-reader-text" for="s"><?php _e( 'Buscar por nombre:', 'woocommerce' ); ?></label>
@@ -39,7 +39,7 @@ $lowest_price = get_post_meta( $lowest->ID, '_price', true );
   <div class="sidebar-mobile show-mobile py-5 px-3">
     <p class="px-0 pb-0">Precio máximo</p>
     <small id="emp-small-price-mobile"><?php echo $highest_price; ?></small>
-    <input id="emp-range-price-mobile" class="emp-range-price mb-3" type="range" min="<?php echo $lowest_price; ?>" max="<?php echo $highest_price; ?>" step="1" onchange="window.location.href = 'http://localhost/wordpress/tienda/?max_price='+this.value">
+    <input id="emp-range-price-mobile" class="emp-range-price mb-3" type="range" min="<?php echo $lowest_price; ?>" max="<?php echo $highest_price; ?>" step="1" onchange="window.location.href =  window.location.href.split('?')[0]+'?max_price='+this.value">
     <p class="px-0 pb-0">Filtrar por nombre</p>
     <?php get_product_search_form();?>
   </div>
