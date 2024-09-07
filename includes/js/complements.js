962b611235262796c2f1cc6d31e2a46636f5d4de
Alan Pereyra <alanpereyra33@gmail.com>
2024-09-07 19:31:24 -0300

    Add countdown support

--- includes/js/complements.js
+++ includes/js/complements.js
@@ -431,3 +431,29 @@ if (wooBankDetails) {
   p.innerHTML = text;
   wooBankDetailItems.parentNode.insertBefore(p, wooBankDetailItems.nextSibling);
 }
+
+// Countdown
+function updateTimer() {
+  future  = Date.parse("September 7, 2024 20:00:00");
+  now     = new Date();
+  diff    = future - now;
+
+  days  = Math.floor( diff / (1000*60*60*24) );
+  hours = Math.floor( diff / (1000*60*60) );
+  mins  = Math.floor( diff / (1000*60) );
+  secs  = Math.floor( diff / 1000 );
+
+  d = days;
+  h = hours - days  * 24;
+  m = mins  - hours * 60;
+  s = secs  - mins  * 60;
+
+  document.getElementById("timer")
+    .innerHTML =
+      '<div>' + d + '<span>Día</span></div>' +
+      '<div>' + h + '<span>Horas</span></div>' +
+      '<div>' + m + '<span>Minutos</span></div>' +
+      '<div>' + s + '<span>Segundos</span></div>' ;
+}
+setInterval('updateTimer()', 1000 );
+
